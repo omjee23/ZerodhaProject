e60aa8f4520bb608e57c8b82bf36fef8d1655a16
Om Jee Pal <om22@navgurukul.org>
2025-02-11 23:40:54 +0530

    complete Abput page

--- frontend/src/LandingPage/About/AboutPage.jsx
+++ frontend/src/LandingPage/About/AboutPage.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import Hero from './Hero';
+import Team from './Team';
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div>
+      <Hero/>
+      <Team/>
+    </div>
   )
 }
 
---
+++ frontend/src/LandingPage/About/Team.jsx
@@ -0,0 +1,35 @@
+
+function Team() {
+  return (
+    <div className="container">
+      <div className="row mt-3">
+        <h1 className="fs-2 text-center">
+         People
+        </h1>
+      </div>
+      <div className="row p-5  mt-5 text-muted" style={{lineHeight: "1.9", fontSize: "17px"}}>
+        <div className="col-6 p-5 text-center">
+            <img src="Media/images/nithinKamath.jpg" alt="nitinKumar" style={{borderRadius: "100%" , width: "55%"}} />
+            <h4 className="mt-5">Nithin Kamath</h4>
+            <h6>Founder, CEO</h6>
+        </div>
+        <div className="col-6 p-5">
+            <p>
+            Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.
+            </p>
+            <p>
+            He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).
+            </p>
+            <p>
+            Playing basketball is his zen
+            </p>
+            <p>
+            Connect on <a className="text-decoration-none" href="">Homepage </a>/<a className="text-decoration-none"href=""> TradingQnA </a>/ <a className="text-decoration-none" href=""> Twitter</a>
+            </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Team
