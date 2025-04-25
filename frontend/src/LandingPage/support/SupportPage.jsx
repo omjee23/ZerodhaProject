0d5f58841cfcbeb451b4f79469a16dac6b2a1db9
omjee23 <om22@navgurukul.org>
2025-04-25 21:20:30 +0530

    completed support page

--- frontend/src/LandingPage/support/SupportPage.jsx
+++ frontend/src/LandingPage/support/SupportPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import CreateTickets from './CreateTickets'
+import Hero from './Hero.jsx'
 function SupportPage() {
   return (
-    <div>SupportPage</div>
+    <div>
+      <Hero/>
+      <CreateTickets/>
+
+    </div>
   )
 }
 
